ea17c1dfa82baa68b8b41891cce8e07d893e0236
Allen Davis <harmonyinceltic@gmail.com>
2015-12-30 20:00:50 +0530

    active admin cofiguration added(incomplete)

--- app/admin/gallery.rb
+++ app/admin/gallery.rb
@@ -13,5 +13,22 @@ ActiveAdmin.register Gallery do
 #   permitted
 # end
 
+form :html => { :enctype => "multipart/form-data"} do |f|
+	f.inputs "Image Uploads" do
+		f.input :image_link, :as => :file, :hint => f.template.image_tag(f.object.photo.url(:medium))
+	end
+	f.buttons
+end
+
+index do
+    # column :image_link
+    column :image_link, :sortable => :image_link do |gallery|
+      div :class => "image_link_class" do
+         gallery.image_link
+      end
+    end
+    
+  actions
+  end
 
 end
