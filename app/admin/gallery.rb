783a4be3b915f6eefe450368e933900e6e90732b
Sreenath B <bsreeinf@gmail.com>
2016-01-07 18:41:16 +0530

    Mail fixes

--- app/admin/gallery.rb
+++ app/admin/gallery.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Gallery do
+	menu priority: 3
 	permit_params :image , :title
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/admin/nsoj_radio.rb
+++ app/admin/nsoj_radio.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register NsojRadio do
-
+	menu priority: 5, label: "Podcasts"
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
