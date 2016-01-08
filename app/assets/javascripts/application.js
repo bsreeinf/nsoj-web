f042e242d980dd6981b004b47e650ee97ad41225
Allen Davis <harmonyinceltic@gmail.com>
2016-01-08 19:16:21 +0530

    Ie fix, arranging all the assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,28 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require_directory .
+//= require jquery.easing.1.3
 //= require bootstrap
+//= require SmoothScroll
+//= require jquery.scrollTo.min
+//= require jquery.localScroll.min
+//= require jquery.viewport.mini
+//= require jquery.countTo
+//= require jquery.appear
+//= require jquery.sticky
+//= require jquery.parallax-1.1.3
+//= require jquery.fitvids
+//= require owl.carousel.min
+//= require isotope.pkgd.min
+//= require imagesloaded.pkgd.min
+//= require jquery.magnific-popup.min
+//= require gmap3.min
+//= require wow.min
+//= require masonry.pkgd.min
+//= require jquery.simple-text-rotator.min
+//= require all
+//= require placeholder
+//= require_directory .
 //= require turbolinks
 
 
