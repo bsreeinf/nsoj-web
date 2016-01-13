137a622315b98e7749153fbb7c7eb5d1d99ebc15
Allen Davis <harmonyinceltic@gmail.com>
2016-01-14 02:41:14 +0530

    three catergories of the tv is also added in the controller and the news view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,13 @@ class BlogsController < InheritedResources::Base
  	@nsojradios=NsojRadio.order("created_at DESC").limit(3)
  	@nsojtvs=NsojTv.order("created_at DESC").limit(3)
 
+ 	# @nsojtvs_no_nuisance = NsojTv.order("created_at DESC").limit(3)
+ 	@nsojtvs_bulleitin = NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 1).limit(3)
+ 	
+ 	@nsojtvs_featured= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 2).limit(3)
+
+ 	@nsojtvs_no_nuisance= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 3).limit(3)
+
  	
 
 
