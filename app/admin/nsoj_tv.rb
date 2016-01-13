e4012d2e3f86550da645f1fc5dda060ac661c6ea
Allen Davis <harmonyinceltic@gmail.com>
2016-01-14 02:47:08 +0530

    minor admin panel fixes

--- app/admin/nsoj_tv.rb
+++ app/admin/nsoj_tv.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register NsojTv do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title, :link
+permit_params :title, :link ,:nsoj_tv_category_id
 #
 # or
 #
