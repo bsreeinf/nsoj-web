1fbe650b811f3aef5c0a7ea1fede56b0dbf1962e
Sreenath B <bsreeinf@gmail.com>
2016-01-19 13:00:44 +0530

    hidden login mechanism

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,11 @@ ActiveRecord::Schema.define(version: 20160118193436) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "contact_subjects", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
