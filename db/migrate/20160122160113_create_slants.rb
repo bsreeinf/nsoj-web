a06c79a7882c14f2dbe011d5cfbcaaae4a92278f
Sreenath B <bsreeinf@gmail.com>
2016-01-22 21:57:44 +0530

    slant migration fix

--- db/migrate/20160122160113_create_slants.rb
+++ db/migrate/20160122160113_create_slants.rb
@@ -1,68 +1,68 @@
 class CreateSlants < ActiveRecord::Migration
   def change
     create_table :slants do |t|
-      t.college_name :string
-      t.phone :string
+      t.string :college_name
+      t.string :phone
 
-      t.rb_name1 :string
-      t.rb_phone1 :string
+      t.string :rb_name1
+      t.string :rb_phone1
       
-      t.rb_name2 :string
-      t.rb_phone2 :string
+      t.string :rb_name2
+      t.string :rb_phone2
       
-      t.rb_name3 :string
-      t.rb_phone3 :string
+      t.string :rb_name3
+      t.string :rb_phone3
       
-      t.rb_name4 :string
-      t.rb_phone4 :string
+      t.string :rb_name4
+      t.string :rb_phone4
       
-      t.rb_name5 :string
-      t.rb_phone5 :string
+      t.string :rb_name5
+      t.string :rb_phone5
       
-      t.rb_name6 :string
-      t.rb_phone6 :string
+      t.string :rb_name6
+      t.string :rb_phone6
 
 
-      t.bn_name1 :string
-      t.bn_phone1 :string
+      t.string :bn_name1
+      t.string :bn_phone1
       
-      t.bn_name2 :string
-      t.bn_phone2 :string
+      t.string :bn_name2
+      t.string :bn_phone2
       
-      t.bn_name3 :string
-      t.bn_phone3 :string
+      t.string :bn_name3
+      t.string :bn_phone3
       
-      t.bn_name4 :string
-      t.bn_phone4 :string
+      t.string :bn_name4
+      t.string :bn_phone4
       
       
-      t.fc_name1 :string
-      t.fc_phone1 :string
+      t.string :fc_name1
+      t.string :fc_phone1
       
-      t.fc_name2 :string
-      t.fc_phone2 :string
+      t.string :fc_name2
+      t.string :fc_phone2
       
-      t.fc_name3 :string
-      t.fc_phone3 :string
-      
-
-      t.tt_name1 :string
-      t.tt_phone1 :string
-
-      t.tt_phone2 :string
-      t.tt_phone3 :string
+      t.string :fc_name3
+      t.string :fc_phone3
       
 
-      t.es_name :string
-      t.es_phone :string
+      t.string :tt_name1
+      t.string :tt_phone1
+
+      t.string :tt_phone2
+      t.string :tt_phone3
       
 
-      t.pj_name :string
-      t.pj_phone :string
+      t.string :es_name
+      t.string :es_phone
       
 
-      t.do_name :string
-      t.do_phone :string
+      t.string :pj_name
+      t.string :pj_phone
+      
+
+      t.string :do_name
+      t.string :do_phone
 
       t.timestamps null: false
     end
