db18a1a3ed8b6b2914a03495f38342d067dd0b4d
Sreenath B <bsreeinf@gmail.com>
2016-04-11 16:35:25 +0530

    Changes and fixes

--- app/admin/admission_form.rb
+++ app/admin/admission_form.rb
@@ -29,6 +29,92 @@ permit_params :user_id,
 #   permitted << :other if resource.something?
 #   permitted
 # end
-
+index :tag => "Admission Forms" do
+        column :user_id
+        column :last_name1
+        column :first_name1 
+        column :middle_name1 
+        column :name_title1 
+        column :last_name2 
+        column :first_name2 
+        column :middle_name2 
+        column :name_title2 
+        column :dob 
+        column :country_of_permanent_residence 
+        column :country_of_birth 
+        column :nationality 
+        column :second_nationality 
+        column :addr_street1 
+        column :addr_city1 
+        column :addr_state1 
+        column :addr_pincode1 
+        column :addr_country1 
+        column :phone1 
+        column :addr_until 
+        column :addr_street2 
+        column :addr_city2 
+        column :addr_state2 
+        column :addr_pincode2 
+        column :addr_country2 
+        column :phone2 
+        column :addr_since 
+        column :email 
+        column :s_university 
+        column :s_subject 
+        column :s_degree 
+        column :s_date_started 
+        column :s_date_to_be_obtained 
+        column :s_expected_gpa 
+        column :r_university 
+        column :r_subject 
+        column :r_degree 
+        column :r_date_to_be_obtained 
+        column :r_expected_gpa 
+        column :a_degree1 
+        column :a_program1 
+        column :a_institution1 
+        column :a_department1 
+        column :a_country1 
+        column :a_degree2 
+        column :a_program2 
+        column :a_institution2 
+        column :a_department2 
+        column :a_country2 
+        column :a_degree3 
+        column :a_program3 
+        column :a_institution3 
+        column :a_department3 
+        column :a_country3 
+        column :a_degree4 
+        column :a_program4 
+        column :a_institution4 
+        column :a_department4 
+        column :a_country4 
+        column :a_degree5 
+        column :a_program5 
+        column :a_institution5 
+        column :a_department5 
+        column :a_country5 
+        column :eh_from_date1 
+        column :eh_to_date1 
+        column :eh_post_held1 
+        column :eh_employer_details1 
+        column :eh_from_date2 
+        column :eh_to_date2 
+        column :eh_post_held2 
+        column :eh_employer_details2 
+        column :eh_from_date3 
+        column :eh_to_date3 
+        column :eh_post_held3 
+        column :eh_employer_details3 
+        column "Assessment - Experience", :assessment_interested 
+        column "Assessment - Interested", :assessment_experience 
+        column :gender 
+        column :visa_required 
+        column :program_of_study 
+        column :currently_studying
+        column :is_submitted
+        actions
+end
 
 end
--- app/models/nsoj_post.rb
+++ app/models/nsoj_post.rb
@@ -5,18 +5,5 @@ class NsojPost < ActiveRecord::Base
                     :convert_options => { :all => '-colorspace RGB -flatten -density 300 -quality 100' }, 
                     :path => ":page_path/:class/:id/:resource_token/:style/:filename"
     validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
-
-   #    after_document_post_process :make_pages
-
-	  # private
-
-	  # def make_pages
-
-	  # 	# Paperclip.run('convert', "-size 100 #{document.queued_for_write[:original].path} #{document.queued_for_write[:original].path}%d.png")
-
-
-
-	  # end
-
   
 end
