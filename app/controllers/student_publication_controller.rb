32c5167b9df3907aaeab89b9d256a4ca90fb8e21
Sreenath B <bsreeinf@gmail.com>
2016-10-17 06:28:29 +0530

    Added nav links for stories and students

--- app/controllers/student_publication_controller.rb
+++ app/controllers/student_publication_controller.rb
@@ -1,15 +1,12 @@
 class StudentPublicationController < ApplicationController
  	def index
-	  	@nsojposts=NsojPost.order("created_at DESC").limit(3)
-	  	@nsojradios=NsojRadio.order("created_at DESC").limit(3)
-	 	@nsojtvs=NsojTv.order("created_at DESC").limit(3)
-
+	  	@nsojposts=NsojPost.order("created_at DESC").limit(4)
+	  	@nsojradios=NsojRadio.order("created_at DESC").limit(4)
+	 	@nsojtvs=NsojTv.order("created_at DESC").limit(4)
 	 	# @nsojtvs_no_nuisance = NsojTv.order("created_at DESC").limit(3)
-	 	@nsojtvs_bulleitin = NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 1).limit(3)
-	 	
-	 	@nsojtvs_featured= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 2).limit(3)
-
-	 	@nsojtvs_no_newsense= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 3).limit(3)
+	 	@nsojtvs_bulleitin = NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 1).limit(4)
+	 	@nsojtvs_featured= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 2).limit(4)
+	 	@nsojtvs_no_newsense= NsojTv.order("created_at DESC").where('nsoj_tv_category_id = ?', 3).limit(4)
 
 	end
 
