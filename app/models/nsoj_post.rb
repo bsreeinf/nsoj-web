5d30479fc0ce5ca3cffa9b4bc861d77a54a5a3f3
Sreenath B <bsreeinf@gmail.com>
2016-10-24 21:41:41 +0530

    Paperclip thumb regeneration for NSoJ Post

--- app/models/nsoj_post.rb
+++ app/models/nsoj_post.rb
@@ -1,6 +1,6 @@
 class NsojPost < ActiveRecord::Base
 	has_attached_file :document,
-                    :styles => { :thumb => ["200x320>", :jpg]},
+                    :styles => { :thumb => ["200x320#", :jpg]},
                     :processors => [:ghostscript, :thumbnail],
                     :convert_options => { :all => '-colorspace RGB -flatten -density 300 -quality 100' }, 
                     :path => ":page_path/:class/:id/:resource_token/:style/:filename"
