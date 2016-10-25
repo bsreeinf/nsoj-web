4f3ffd2c97c51c912437a7a5d685edc4825f7f3c
Sreenath B <bsreeinf@gmail.com>
2016-10-25 14:57:27 +0530

    s3 alias url removed for post

--- app/models/nsoj_post.rb
+++ app/models/nsoj_post.rb
@@ -3,8 +3,6 @@ class NsojPost < ActiveRecord::Base
                     :styles => { :thumb => ["200x320#", :jpg]},
                     :processors => [:ghostscript, :thumbnail],
                     :convert_options => { :all => '-colorspace RGB -flatten -density 300 -quality 100' }, 
-                    :s3_host_alias => "nsoj.in",
-					:url => ":s3_alias_url",
                     :path => ":page_path/:class/:id/:resource_token/:style/:filename"
 
     validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf)}
