68a5e1aed16e28d0474b7a310d72bb2483f3a3f7
Sreenath B <bsreeinf@gmail.com>
2016-11-29 22:16:41 +0530

    Story category timestamp spacing fix

--- app/admin/nsoj_tv.rb
+++ app/admin/nsoj_tv.rb
@@ -1,17 +1,8 @@
 ActiveAdmin.register NsojTv do
-	menu priority: 6, label: "Videos", parent: "NSoJ TV"
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
+menu priority: 6, label: "Videos", parent: "NSoJ TV"
+
 permit_params :title, :link ,:nsoj_tv_category_id, video_authors_attributes: [:id, :student_id, :nsoj_tv_id]
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+
 
 	form :html => { :enctype => "multipart/form-data"} do |f|
 
@@ -52,59 +43,4 @@ permit_params :title, :link ,:nsoj_tv_category_id, video_authors_attributes: [:i
 	end
 end
 
- #  1 |                 3
- #  3 |                 4
- #  4 |                 4
- #  5 |                 5
- #  6 |                 3
- #  7 |                 3
- #  8 |                 5
- #  9 |                 3
- # 10 |                 3
- # 11 |                 3
- # 12 |                 5
- # 13 |                 5
- # 14 |                 3
- # 15 |                 3
- # 16 |                 5
- # 17 |                 5
- # 18 |                 4
- # 19 |                 5
- # 20 |                 3
- # 21 |                 5
- # 22 |                 5
- # 23 |                 5
- # 24 |                 3
- # 25 |                 3
- # 26 |                 3
- # 27 |                 5
- # 28 |                 4
- # 29 |                 3
- # 30 |                 3
- # 31 |                 5
- # 32 |                 5
- # 33 |                 5
- # 34 |                 6
- # 35 |                 3
- # 36 |                 3
- # 37 |                 3
- # 38 |                 5
- # 42 |                 5
- # 43 |                 3
- # 44 |                 3
- # 45 |                 2
- # 46 |                 2
- # 47 |                 5
- # 48 |                 5
- # 49 |                 5
- # 50 |                 3
- # 51 |                 8
- # 52 |                 3
- # 53 |                 6
- # 54 |                 3
- # 55 |                 5
- # 56 |                 2
- # 57 |                 5
- # 58 |                 3
- # 59 |                 5
- # 60 |                 3
+ 
