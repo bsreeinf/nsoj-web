0fd12ca861365e8c5630c7c9fd9889d8546e94a3
Sreenath B <bsreeinf@gmail.com>
2017-01-21 00:24:08 +0530

    minimal change to try stuff

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,26 +4,29 @@ class ContactsController < ApplicationController
 		
 
 		@contact = Contact.new(contact_params)
-
+		  if @contact.email != @contact.message
 		
-	      if @contact.save
+		      if @contact.save
 
-	        ContactMailer.contact_created(
-	        	@contact.name,
-	        	@contact.email,
-	        	@contact.city,
-	        	@contact.phone,
-	        	@contact.contact_subject.title,
-	        	@contact.university,
-	        	@contact.message
-	        ).deliver
-	        redirect_to root_path, notice: 'Thanks for contacting us. We will get back to you shortly.' 
-	        
-	      else
-	        redirect_to root_path, notice: 'Something went wrong. Please try contacting us later'
-	    
-	      end
-	
+		        ContactMailer.contact_created(
+		        	@contact.name,
+		        	@contact.email,
+		        	@contact.city,
+		        	@contact.phone,
+		        	@contact.contact_subject.title,
+		        	@contact.university,
+		        	@contact.message
+		        ).deliver
+		        redirect_to root_path, notice: 'Thanks for contacting us. We will get back to you shortly.' 
+		        
+		      else
+		        redirect_to root_path, notice: 'Something went wrong. Please try contacting us later'
+		    
+		      end
+		  else 
+		  	else
+		        redirect_to root_path, notice: 'Something went wrong. Please try contacting us later'
+		end
 
 
 
