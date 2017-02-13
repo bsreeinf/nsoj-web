c8ff1af174e62e8b0c354c9ad22c3bc055c9fc10
Sreenath B <bsreeinf@gmail.com>
2017-02-13 11:57:53 +0530

    route fix for ck

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   resources :stories
-  mount Ckeditor::Engine => '/ckeditor'
   mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdf'
   
   resources :students
