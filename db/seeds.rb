2512ce7f7cc0b389fc4ab245be9274e56e75e401
Sreenath B <bsreeinf@gmail.com>
2018-02-01 13:30:11 +0530

    asset_sync for S3 configured

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
- # AuthorType.create!(	title: "Story Byline"	)
- # AuthorType.create!(	title: "Video Byline"	)
+ AuthorType.create!(	title: "Story Byline"	)
+ AuthorType.create!(	title: "Video Byline"	)
 
-# ContactSubject.create!(	title: "Admissions Office"	)
-# ContactSubject.create!(	title: "Programmes"	)
-# ContactSubject.create!(	title: "NSoJ Visits"	)
-# ContactSubject.create!(	title: "Internship and Career Services Centre")
+ContactSubject.create!(	title: "Admissions Office"	)
+ContactSubject.create!(	title: "Programmes"	)
+ContactSubject.create!(	title: "NSoJ Visits"	)
+ContactSubject.create!(	title: "Internship and Career Services Centre")
 
-# Batch.create!( year: 2016)
-# Batch.create!( year: 2017)
+Batch.create!( year: 2016)
+Batch.create!( year: 2017)
 
-# StoryCategory.create!( title: "International")
-# StoryCategory.create!( title: "Politics")
-# StoryCategory.create!( title: "Bangalore")
-# StoryCategory.create!( title: "Sports") 
-# StoryCategory.create!( title: "Lifestyle")
-# StoryCategory.create!( title: "Business")
-# StoryCategory.create!( title: "Uncategorised")
+StoryCategory.create!( title: "International")
+StoryCategory.create!( title: "Politics")
+StoryCategory.create!( title: "Bangalore")
+StoryCategory.create!( title: "Sports") 
+StoryCategory.create!( title: "Lifestyle")
+StoryCategory.create!( title: "Business")
+StoryCategory.create!( title: "Uncategorised")
 
-# AdminUser.create!(email: 'admin@nsoj.in', password: 'NSoJ_Admin@123', password_confirmation: 'NSoJ_Admin@123')
-# User.create!( 
-# 	name: "Sreenath B", 
-# 	email: "bsreeinf@gmail.com",
-# 	phone: "+91 805 023 6035", 
-# 	password: "qwertyui", 
-# 	password_confirmation: "qwertyui", 
-# 	address: "521, 1st Cross, XYZ Street", 
-# 	activated: true,
-# 	city: "Bangalore", 
-# 	pincode: "560 033", 
-# 	state: "Karnataka", 
-# 	country: "India"
-# )
+AdminUser.create!(email: 'admin@nsoj.in', password: 'NSoJ_Admin@123', password_confirmation: 'NSoJ_Admin@123')
+User.create!( 
+	name: "Sreenath B", 
+	email: "bsreeinf@gmail.com",
+	phone: "+91 805 023 6035", 
+	password: "qwertyui", 
+	password_confirmation: "qwertyui", 
+	address: "521, 1st Cross, XYZ Street", 
+	activated: true,
+	city: "Bangalore", 
+	pincode: "560 033", 
+	state: "Karnataka", 
+	country: "India"
+)
 
